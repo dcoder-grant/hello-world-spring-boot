9158f2c695556d8a3d742a2735773edd1891ceb8
dcoder-grant <47870112+dcoder-grant@users.noreply.github.com>
2021-06-04 07:43:15 -0400

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World updated";
+        return "Hello World";
     }
 
     public static void main(String[] args) throws Exception {
